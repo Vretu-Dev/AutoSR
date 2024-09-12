e16e15a09653c11a84088b3afd420147dfd06969
Vretu <82831707+Vretu-Dev@users.noreply.github.com>
2024-09-12 22:13:58 +0200

    Initial release

---
+++ AutoSR/AutoSR.cs
@@ -0,0 +1,70 @@
+﻿using Exiled.API.Features;
+using System.Timers;
+using System;
+
+namespace AutoSR
+{
+    public class AutoSR : Plugin<Config>
+    {
+        public override string Name => "AutoSR";
+        public override string Author => "Vretu";
+        public override string Prefix { get; } = "AutoSR";
+        public override Version Version => new Version(1, 0, 0);
+        public override Version RequiredExiledVersion { get; } = new Version(8, 9, 8);
+
+        private Timer restartTimer;
+        private Timer broadcastTimer;
+
+        public override void OnEnabled()
+        {
+            Exiled.Events.Handlers.Server.RoundStarted += OnRoundStarted;
+            base.OnEnabled();
+        }
+        public override void OnDisabled()
+        {
+            Exiled.Events.Handlers.Server.RoundStarted -= OnRoundStarted;
+
+            if (restartTimer != null)
+            {
+                restartTimer.Stop();
+                restartTimer.Dispose();
+            }
+            if(broadcastTimer != null)
+            {
+                broadcastTimer.Stop();
+                broadcastTimer.Dispose();
+            }
+            base.OnDisabled();
+        }
+        private void OnRoundStarted()
+        {
+            restartTimer = new Timer(Config.SecondsUntilRestart * 1000);
+            restartTimer.Elapsed += OnTimerElapsed;
+            restartTimer.AutoReset = false;
+            restartTimer.Start();
+
+            if (Config.SecondsUntilRestart > 30)
+            {
+                broadcastTimer = new Timer((Config.SecondsUntilRestart - 30) * 1000);
+                broadcastTimer.Elapsed += OnBroadcastElapsed;
+                broadcastTimer.AutoReset = false;
+                broadcastTimer.Start();
+            }
+            Log.Info($"Timer set for {Config.SecondsUntilRestart} seconds to restart the round.");
+        }
+        private void OnBroadcastElapsed(object sender, ElapsedEventArgs e)
+        {
+            broadcastTimer.Stop();
+            broadcastTimer.Dispose();
+            Log.Info("Server will be restarted within 30 seconds.");
+            Map.Broadcast(10, Config.BroadcastMessage, Broadcast.BroadcastFlags.Normal, true);
+        }
+        private void OnTimerElapsed(object sender, ElapsedEventArgs e)
+        {
+            restartTimer.Stop();
+            restartTimer.Dispose();
+            Log.Info("Restarting round.");
+            Server.ExecuteCommand(Config.Command);
+        }
+    }
+}
---
+++ AutoSR/config.cs
@@ -0,0 +1,17 @@
+﻿using Exiled.API.Interfaces;
+using System.ComponentModel;
+
+namespace AutoSR
+{
+    public class Config : IConfig
+    {
+        public bool IsEnabled { get; set; } = true;
+        public bool Debug { get; set; } = false;
+        [Description("Time after server will be restarted in seconds")]
+        public int SecondsUntilRestart { get; set; } = 1800;
+        [Description("Command to be executed eg: 'sofrestart','restart','roundrestart'.")]
+        public string Command { get; set; } = "softrestart";
+        [Description("Broadcast to be displayed.")]
+        public string BroadcastMessage { get; set; } = "Server will be restarted within 30 seconds";
+    }
+}
