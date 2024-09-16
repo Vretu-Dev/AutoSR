4c2911df6db06188c69c45c9a9bda8ec50d8d8d1
Vretu <82831707+Vretu-Dev@users.noreply.github.com>
2024-09-17 01:05:06 +0200

    1.0.1

--- AutoSR/AutoSR.cs
+++ AutoSR/AutoSR.cs
@@ -9,7 +9,7 @@ namespace AutoSR
         public override string Name => "AutoSR";
         public override string Author => "Vretu";
         public override string Prefix { get; } = "AutoSR";
-        public override Version Version => new Version(1, 0, 0);
+        public override Version Version => new Version(1, 0, 1);
         public override Version RequiredExiledVersion { get; } = new Version(8, 9, 8);
 
         private Timer restartTimer;
